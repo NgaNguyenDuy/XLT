c2e4095fcbdc39f9a8eaec1bde9e8c0f3816b3bf
NgaNguyenDuy <kyo1508@gmail.com>
2014-10-27 13:32:39 +0700

    update some dependencies

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var vf = require('vf');
 var boot = require('vsoft-boot');
 
 var app = vf();
-
-
 app.use(vf.favicon());
+
+
 boot(app, __dirname);
 
 
@@ -13,13 +13,13 @@ boot(app, __dirname);
 app.use(vf.urlNotFound());
 app.use(vf.errorHandler());
 
-
 app.start = function() {
     return app.listen(function() {
         app.emit('started');
         console.log('Server was running at %s', app.get('url'));
     });
 };
+
 
 if(require.main === module) {
     app.start();
