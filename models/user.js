9048709bfb3a66469b4c0ea6b13ada838be65409
NgaNguyenDuy <kyo1508@gmail.com>
2014-11-01 09:03:28 +0700

    update role

--- models/user.js
+++ models/user.js
@@ -1,3 +1,27 @@
-module.exports = function(User) {
+var debug = require('debug')('boot:create-role');
 
+
+module.exports = function(userDef) {
+    
+    // userDef.afterRemote('create', function(ctx, user, next) {
+        
+
+    // });
+        
+        
+        
+        // console.log(user);
+        // var options = {
+        //     type: 'email',
+        //     to: user.email,
+        //     from: 'noreply@myapp.com',
+        //     subject: 'Thanks for Registering at FooBar',
+        //     text: 'Please verify your email address: {href}',
+        //     template: 'path/to/verify.ejs',
+        //     redirect: '/',
+        //     verifyHref: 'http://myapp.com/api/my-users/confirm'
+        // };
+        // user.verify(options, next);
+//    });
+    
 };
--- server.js
+++ server.js
@@ -1,16 +1,69 @@
-var vf = require('vf');
-var boot = require('vsoft-boot');
+var vf = require('vf'),
+    boot = require('vsoft-boot'),
+    debug = require('debug')('xlt:vframe'),
+    app = vf();
 
-var app = vf();
+// request pre-processing middleware
+app.use(vf.compress());
+
+// using Favicon
 app.use(vf.favicon());
 
 // -- Add your pre-processing middleware here --
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Bootstrap application
 boot(app, __dirname);
 
 
+// The access token is only available after boot
+app.use(vf.token({
+    model: app.models.accessToken
+}));
+
+app.use(vf.cookieParser(app.get('cookieSecretXLTPrj')));
+
+app.use(vf.session({
+    secret: 'xltprj2015',
+    saveUninitialized: true,
+    resave: true
+}));
+
+
+
+
+// User.create({email: 'foo@bar.com', password: 'bar'}, function(err, user) {
+//   console.log(user);
+// }); 
+
+var Role = app.models.Role;
+var RoleMapping = app.models.RoleMapping;
+
+Role.create({
+    name: 'admin'
+}, function(err, role) {
+    if (err) return debug(err);
+    debug(role);
+    role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: "544e2d915a33f4834346cc9a"
+    }, function(err, principal) {
+        if (err) return debug(err);
+        debug(principal);
+    });
+});
+
+
+
+
+
+// User.login({"email": "kyo1508@gmail.com", "password": "123456"}, function(err, accessToken) {
+//     console.log(accessToken);
+//     // User.logout(accessToken.id, function(err) {
+//     //     console.log('Logout successful');
+//     // });
+// });
 
 
 app.use(vf.urlNotFound());
